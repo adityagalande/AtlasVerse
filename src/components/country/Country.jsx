23920b429e4417c940f79116d6cda21fbb70e944
Aditya Galande <56816119+adityagalande@users.noreply.github.com>
2024-07-09 10:52:47 +0000

    Done with router & api call

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useId, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 // component
 import Card from "../card/Card";
@@ -11,13 +12,15 @@ function Country() {
     return (
         <div className="flex justify-center items-center flex-wrap">
             {res.map((item) => (
-                <Card
-                    countryName={item.name.common}
-                    countryImg={item.flags.png}
-                    countryPopulation={item.population}
-                    countryRegion={item.region}
-                    countryLanguage={item.name.common == "Antarctica" ? "NA" : Object.keys(item.languages).length > 1 ? Object.values(item.languages)[1] : Object.values(item.languages)[0]}
-                    countryCurrency={item.region == "Antarctic" ? "NA" : (Object.values(item.currencies))[0].name ? (Object.values(item.currencies))[0].name : "NA"} />
+                <NavLink to={`/details/${item.name.official}`} >
+                    <Card
+                        countryName={item.name.common}
+                        countryImg={item.flags.png}
+                        countryPopulation={item.population}
+                        countryRegion={item.region}
+                        countryLanguage={item.name.common == "Antarctica" ? "NA" : Object.keys(item.languages).length > 1 ? Object.values(item.languages)[1] : Object.values(item.languages)[0]}
+                        countryCurrency={item.region == "Antarctic" ? "NA" : (Object.values(item.currencies))[0].name ? (Object.values(item.currencies))[0].name : "NA"} />
+                </NavLink>
             ))}
         </div>
     );
--- src/components/country_details/CountryDetails.jsx
+++ src/components/country_details/CountryDetails.jsx
@@ -11,28 +11,30 @@ function CountryDetails() {
     return(
         <div className="flex justify-center items-center flex-wrap">
             {countryData.map((item) => (
-                // <div key={uniqueKey} className="flex flex-wrap justify-center items-center p-8 border">
-                //     <Link to={`/card`}>{item.name.common}</Link>
-                //     {/* <h1 to={`/card`}>{item.name.common}</h1> */}
-                //     <div><img src={item.flags.png} alt="Img" className="h-12 p-2" /></div>
-                // </div>
                 
-                <div className="w-full h-full bg-orange-200 justify-center items-center">
+                <div className="w-full h-full bg-yellow-50 justify-center items-center">
                     <div className="p-8 text-center font-bold text-4xl">
                         <h1>{item.name.common}</h1>
                     </div>
 
                     <div className="text-left font-semibold text-xl p-4" >
                         <h3>Names</h3>
+                        <p>Common: {item.name.common}</p>
+                        <p>Official: {item.name.official}</p>
                     </div>
                     <div className="text-left font-semibold text-xl p-4" >
                         <h3>Language</h3>
+                        <p>Language: {item.name.common == "Antarctica" ? "NA" : Object.keys(item.languages).length > 1 ? Object.values(item.languages)[1] : Object.values(item.languages)[0]}</p>
                     </div>
                     <div className="text-left font-semibold text-xl p-4" >
                         <h3>Geography</h3>
+                        <p>Region: {item.region}</p>
+                        <p>Sub-Region: {item.subregion}</p>
                     </div>
                     <div className="text-left font-semibold text-xl p-4" >
                         <h3>Codes</h3>
+                        <p>ISO 3166-1 alpha-2: {item.cca2}</p>
+                        <p>ISO 3166-1 alpha-3: {item.cca3}</p>
                     </div>
 
                     <div className="p-8">
